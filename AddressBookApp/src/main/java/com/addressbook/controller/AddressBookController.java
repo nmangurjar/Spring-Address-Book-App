d7a308a6a6ea453b7c188dd5cd6ffc97d6f00e0c
nmangurjar <131614724+nmangurjar@users.noreply.github.com>
2025-03-06 13:43:23 +0530

    [Naman] Add. Implemented Service Layer to manage business logic

---
+++ AddressBookApp/src/main/java/com/addressbook/controller/AddressBookController.java
@@ -0,0 +1,46 @@
+package com.addressbook.controller;
+import com.springboot.addressbook.dto.AddressBookDTO;
+import com.springboot.addressbook.model.AddressBook;
+import org.springframework.http.ResponseEntity;
+import com.springboot.addressbook.service.AddressBookService;
+import org.springframework.web.bind.annotation.*;
+import java.util.Map;
+
+
+@RestController
+@RequestMapping("/addressbook")
+public class AddressBookController {
+    private final AddressBookService addressBookService;
+
+    public AddressBookController(AddressBookService addressBookService) {
+        this.addressBookService = addressBookService;
+    }
+
+    @GetMapping
+    public ResponseEntity<Map<Integer, AddressBook>> getAllEntries() {
+        return ResponseEntity.ok(addressBookService.getAllEntries());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<AddressBook> getEntryById(@PathVariable int id) {
+        AddressBook entry = addressBookService.getEntryById(id);
+        return entry != null ? ResponseEntity.ok(entry) : ResponseEntity.notFound().build();
+    }
+
+    @PostMapping
+    public ResponseEntity<String> addEntry(@RequestParam int id, @RequestBody AddressBookDTO dto) {
+        return ResponseEntity.ok(addressBookService.addEntry(id, dto));
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<String> updateEntry(@PathVariable int id, @RequestBody AddressBookDTO dto) {
+        String response = addressBookService.updateEntry(id, dto);
+        return response.equals("Entry not found") ? ResponseEntity.notFound().build() : ResponseEntity.ok(response);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<String> deleteEntry(@PathVariable int id) {
+        String response = addressBookService.deleteEntry(id);
+        return response.equals("Entry not found") ? ResponseEntity.notFound().build() : ResponseEntity.ok(response);
+    }
+}
